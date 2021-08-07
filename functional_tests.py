fee5f2a72896aecde8271ad610639aae7534f6d1
Poirot-World <xuhuizhi612@gmail.com>
2021-08-07 23:38:49 +0800

    use unittest and comment

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,14 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):  #打开浏览器，在测试之前运行
+        self.browser = webdriver.Chrome()
+    def tearDown(self) -> None:  #关闭浏览器，在测试之后运行
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test!') #提醒测试结束了
+
+if __name__ == '__main__': #检查自己是都在命令行中运行，而不是在其他脚本中导入
+    unittest.main()
