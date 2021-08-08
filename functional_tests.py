bc4172c0b679333f39be73232f03a916c987e46b
Poirot-World <xuhuizhi612@gmail.com>
2021-08-08 21:57:35 +0800

    add 2 to do item but code need refactor

--- functional_tests.py
+++ functional_tests.py
@@ -22,14 +22,27 @@ class NewVisitorTest(unittest.TestCase):
         # Selenium中输入内容的方法：send_keys
         inputbox.send_keys("Buy peacock feathers")
         #Keys的作用是发送回车键等特殊的案件
+        #按下回车键后，页面更新了，待办事项中表格显示了"1：Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)#按下回车键之后页面会刷新，time.slepp的作用是等待页面加载完毕
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1:Buy peacock features' for row in rows),"New to-do item did not appear in table")
+        # self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows),
+        #                 f"New to-do item did not appear in table. Contents were:\n {table.text}")
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         #她输入了"Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        # self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)#页面更新，清单中有两个待办事项了
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
+
 
         self.fail('Finish the test!') #提醒测试结束了
 
--- lists/views.py
+++ lists/views.py
@@ -6,4 +6,9 @@ def home_page(request):
     #return HttpResponse('<html><title>To-Do lists</title></html>')
     #render函数：django会自动在所有的应用目录中搜索名为templates的文件夹
     #然后根据模板中的内容构建一个HttpResponse对象
-    return render(request,'home.html') #第一个参数是请求对象，第二个参数是渲染的模板名
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
+    # 第一个参数是请求对象，第二个参数是渲染的模板名，第三个参数是一个字典，把模板变量的名称映射在值上
+    return render(request,'home.html',{'new_item_text':request.POST.get('item_text','')})
+#dic.get用法：如果字典里没有指定"item_text"，那么返回''
+
