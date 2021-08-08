8af7bc3b30be831601db05769beb793d94bc3572
Poirot-World <xuhuizhi612@gmail.com>
2021-08-08 17:53:38 +0800

    add render_to_string and compare

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 # Create your tests here.
@@ -15,6 +16,8 @@ class HomePageTest(TestCase):
         request = HttpRequest() #创建了一个HttpRequest对象。用户请求网页，django中看到的就是这个对象
         response = home_page(request)#把这个httprequest对象传给home_page视图，得到响应。
         html = response.content.decode('utf8')#提取响应的.content，得到的结果是原始字节，即0和1。
+        expected_html = render_to_string('home.html') #用render_to_String手动渲染
+        self.assertEqual(html,expected_html)
         # decode用来把原始字节转换成发给用户的html字符串
         self.assertTrue(html.startswith('<html>')) #希望响应以<html>标签开头，并在结尾处关闭该标签
         self.assertIn('<title>To-Do lists</title>', html)#希望响应中有一个<title>标签，内容包含单词To-Do Lists
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    #return HttpResponse('<html><title>To-Do lists</title></html>')
+    #render函数：django会自动在所有的应用目录中搜索名为templates的文件夹
+    #然后根据模板中的内容构建一个HttpResponse对象
+    return render(request,'home.html') #第一个参数是请求对象，第二个参数是渲染的模板名
