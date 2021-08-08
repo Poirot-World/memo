a75739073c25951bc16a1c27802198709030a881
Poirot-World <xuhuizhi612@gmail.com>
2021-08-08 22:06:32 +0800

    refactor two to do items

--- functional_tests.py
+++ functional_tests.py
@@ -7,6 +7,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Chrome()
     def tearDown(self) -> None:  #关闭浏览器，在测试之后运行
         self.browser.quit()
+    #赋值方法，不以test开头不会被测试
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
         #这个网站有一个在线待办事项应用，去瞧瞧
         self.browser.get('http://localhost:8000')
@@ -25,12 +30,13 @@ class NewVisitorTest(unittest.TestCase):
         #按下回车键后，页面更新了，待办事项中表格显示了"1：Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)#按下回车键之后页面会刷新，time.slepp的作用是等待页面加载完毕
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
         # self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows),
         #                 f"New to-do item did not appear in table. Contents were:\n {table.text}")
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        #self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         #她输入了"Use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -38,10 +44,13 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)#页面更新，清单中有两个待办事项了
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
+        #self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
 
 
         self.fail('Finish the test!') #提醒测试结束了
