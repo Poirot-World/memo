e9c6c8a07557f7558a19d351c26a43475c302136
Poirot-World <xuhuizhi612@gmail.com>
2021-08-13 20:59:56 +0800

    first steps of FT for layout+ styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -121,5 +121,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta = 10)
+
+        #新建一个清单，看到输入框居中显示
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,512,delta = 10)
+
 # if __name__ == '__main__': #检查自己是都在命令行中运行，而不是在其他脚本中导入
 #     unittest.main()
